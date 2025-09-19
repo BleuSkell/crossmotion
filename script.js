1b34e4af33332819699b5fcac915464cc0c2ed8c
Kyano Sowirono <kyano@crossmotion.nl>
2025-09-19 16:59:16 +0200

    begun on the responsive navbar

--- script.js
+++ script.js
@@ -80,4 +80,10 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     });
+
+    const menuToggle = document.getElementById("menu-toggle");
+    const mobileMenu = document.getElementById("mobile-menu");
+    menuToggle.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
 });
